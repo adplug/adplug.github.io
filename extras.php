48876638e324fa6d17010205beb8bf699c97f722
Simon Peter <dn.tlp@gmx.net>
2005-06-01 04:11:32 -0700

    Added links and one more extra package.

--- extras.php
+++ extras.php
@@ -2,7 +2,8 @@
 <?php require("config.php"); ?>
 
 <html>
-<?php $doctitle = "A free AdLib sound player library"; include("header.php"); ?>
+<?php $doctitle = "A free AdLib sound player library";
+include("header.php"); ?>
 <body>
 
 <?php include("menu.php"); ?>
@@ -11,29 +12,44 @@
 <tr><td>
 <a name="extras"><h1>Extras:</h1></a>
 
-<p>Following are some extra packages, you may find useful when using AdPlug.</p>
+<p>Following are some extra packages, you may find useful when using
+AdPlug.</p>
 
 <table>
 <tr><td><b>Package:</b></td><td><b>Description:</b></td></tr>
+<tr><td><a href="http://wlan.kewl.org/fm801/">Using the Forte Media
+Fm801 FM Synth with AdPlug on Windows XP</a></td>
+<td>Darron Broad wrote a nice tutorial on how to get AdPlug to produce
+sound on soundcards with a Forte Media Fm801 FM chip on Windows XP,
+along with a really neat mixer application that is needed in order to
+achieve this.</td></tr>
 <tr><td><a href="files/sb16switch.zip">SB FM Switch</a></td>
-<td>A workaround for Sound Blaster users, who can't get the real OPL2 output with the AdPlug Winamp2 input
-plugin to work. This is a known problem with the Creative FM driver. This workaround is provided by
+<td>A workaround for Sound Blaster users, who can't get the real OPL2
+output with the AdPlug Winamp input plugin to work. This is a known
+problem with the Creative FM driver. This workaround is provided by
 Zbigniew Luszpinski.</td></tr>
 <tr><td><a href="files/examples.zip">Example Tunes</a></td>
-<td>Some packaged example tunes by various composers to demonstrate AdPlug's capabilities. The copyright
-of the included modules relies to their corresponding authors.</td></tr>
+<td>Some packaged example tunes by various composers to demonstrate
+AdPlug's capabilities. The copyright of the included modules relies to
+their corresponding authors.</td></tr>
 </table>
 </td></tr>
 
 <tr><td>
 <a name="links"><h1>Links:</h1></a>
-<p>Following is a list of websites connected to the AdPlug project or AdLib in general:</p>
+<p>Following is a list of websites connected to the AdPlug project or
+AdLib in general:</p>
 
 <ul>
-<li><a href="http://chiptunes.back2roots.org">chiptunes.back2roots.org</a> - the world's biggest adlib
-music archive.</li>
-<li><a href="http://adlib.superfighter.com">adlib.superfighter.com</a> - has very nice .raw rips of some
-really old gems.</li>
+<li><a
+href="http://chiptunes.back2roots.org">chiptunes.back2roots.org</a> -
+the world's biggest adlib music archive</li>
+<li><a href="http://adlib.superfighter.com">adlib.superfighter.com</a>
+- has very nice .raw rips of some really old gems</li>
+<li><a
+href="http://mitglied.lycos.de/borgnumberone/misc/adlib/">mitglied.lycos.de/borgnumberone/misc/adlib/</a>
+- besides a nice introduction into the world of adlib sound grabbing,
+this page hosts RAWEDIT, the only .raw adlib file editor in existence</li>
 </ul>
 </td></tr>
 

629e56f428723bbe7849f2a01af893ab96a3628b
Simon Peter <dn.tlp@gmx.net>
2006-09-18 11:46:49 -0700

    Updated for AdPlay/DOS release.

--- menu.php
+++ menu.php
@@ -13,7 +13,7 @@
 <table class="small" width="100%">
 <tr><td>Core library</td><td align=right><b>2.0.1</b></td></tr>
 <tr><td>AdPlug/Winamp</td><td align=right><b>1.7</b></td></tr>
-<tr><td>AdPlay/DOS</td><td align=right><b>1.5</b></td></tr>
+<tr><td>AdPlay/DOS</td><td align=right><b>1.6</b></td></tr>
 <tr><td>AdPlay/UNIX</td><td align=right><b>1.5</b></td></tr>
 <tr><td>AdPlug/XMMS</td><td align=right><b>1.2</b></td></tr>
 </table>
